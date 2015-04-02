6972ae72961f3a7d63e5a47e1a823e136df32256
luoyangzhang <zhangguanqing@luoyangzhangdeMacBook-Pro.local>
2015-04-02 15:57:08 +0800

    Initial Commit

---
+++ 微学院/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  微学院
+//
+//  Created by luoyangzhang on 15/4/2.
+//  Copyright (c) 2015年 luoyangzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 微学院/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  微学院
+//
+//  Created by luoyangzhang on 15/4/2.
+//  Copyright (c) 2015年 luoyangzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
